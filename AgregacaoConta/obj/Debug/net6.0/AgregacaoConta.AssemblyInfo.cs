ba05dc373fa6402b9f8bef679aca75e4aa39aa43
4LLK4ZZ <allkazzcoder@gmail.com>
2024-06-03 19:27:48 -0300

    0306

--- AgregacaoConta/obj/Debug/net6.0/AgregacaoConta.AssemblyInfo.cs
+++ AgregacaoConta/obj/Debug/net6.0/AgregacaoConta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgregacaoConta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29235b93418f2d4d194382aa5dd7b4674fc1adfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa782364a2a8b1e26b15e11346764963d4528ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgregacaoConta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgregacaoConta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
